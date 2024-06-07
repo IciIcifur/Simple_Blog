6d689672760972141f0c2a96d8501b4cf620aa9a
IciIcifur <iciicifur@yandex.ru>
2024-06-07 11:02:59 +0300

    Trying formatting checks

--- blog/frontend/src/main.ts
+++ blog/frontend/src/main.ts
@@ -4,4 +4,6 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
+
+
 );
--- simpleBlog/schema.py
+++ simpleBlog/schema.py
@@ -6,8 +6,11 @@ class Query(schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(schema.Mutation, graphene.ObjectType):
+class Mutation(schema.Mutation,   graphene.ObjectType):
     pass
+
+
+
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
