7c6e6c9a68c768b67059eaf4c6fe3018af50cebb
IciIcifur <iciicifur@yandex.ru>
2024-06-07 12:19:43 +0300

    Trying formatting checks

--- simpleBlog/schema.py
+++ simpleBlog/schema.py
@@ -6,11 +6,8 @@ class Query(schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(schema.Mutation,   graphene.ObjectType):
+class Mutation(schema.Mutation, graphene.ObjectType):
     pass
-
-
-
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
