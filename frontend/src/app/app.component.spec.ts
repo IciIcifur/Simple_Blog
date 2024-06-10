3bf4920522335271d3abeb8ad5b384d3cbf20752
IciIcifur <iciicifur@yandex.ru>
2024-06-10 17:22:10 +0300

    Trying tests action

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,21 +1,20 @@
-import {TestBed} from '@angular/core/testing';
-import {AppComponent} from './app.component';
-import {appConfig} from "./app.config";
-import {importProvidersFrom} from "@angular/core";
-import {Apollo} from "apollo-angular";
-import {ApolloProvider} from "@apollo/client";
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { appConfig } from './app.config';
+import { importProvidersFrom } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { ApolloProvider } from '@apollo/client';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [appConfig.providers]
+      providers: [appConfig.providers],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(
-      AppComponent);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
@@ -30,8 +29,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'ОтДуши',
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('ОтДуши');
   });
 });
--- frontend/src/app/post/post.service.spec.ts
+++ frontend/src/app/post/post.service.spec.ts
@@ -1,22 +1,26 @@
-import {TestBed} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-import {PostService} from './post.service';
-import {appConfig} from "../app.config";
-import {firstValueFrom, map} from "rxjs";
-
+import { PostService } from './post.service';
+import { appConfig } from '../app.config';
+import { firstValueFrom, map } from 'rxjs';
 
 describe('PostService', () => {
   let service: PostService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({providers: [appConfig.providers]});
+    TestBed.configureTestingModule({ providers: [appConfig.providers] });
     service = TestBed.inject(PostService);
   });
 
   async function getInitialLength() {
-    let initialLength = await firstValueFrom(service.allPosts$.pipe(map((posts) => posts.length)));
+    let initialLength = await firstValueFrom(
+      service.allPosts$.pipe(map((posts) => posts.length)),
+    );
     if (!initialLength) {
-      await service.createPost({content: "Test content", title: "Test title"})
+      await service.createPost({
+        content: 'Test content',
+        title: 'Test title',
+      });
       initialLength += 1;
     }
     return initialLength;
@@ -27,25 +31,49 @@ describe('PostService', () => {
   });
 
   it('should add posts', async () => {
-    await service.createPost({content: "Test content", title: "Test title"})
-    service.allPosts$.subscribe((posts) => expect(posts.find((post) =>
-      post.title == "Test title" && post.content == "Test content")).toBeTruthy())
-  })
+    await service.createPost({ content: 'Test content', title: 'Test title' });
+    service.allPosts$.subscribe((posts) =>
+      expect(
+        posts.find(
+          (post) =>
+            post.title == 'Test title' && post.content == 'Test content',
+        ),
+      ).toBeTruthy(),
+    );
+  });
 
   it('should remove posts', async () => {
     let initialLength = await getInitialLength();
 
-    let firstPostId = await firstValueFrom(service.allPosts$.pipe(map((posts) => posts[0].id)))
-    await service.deletePost(firstPostId)
-    let newLength = await firstValueFrom(service.allPosts$.pipe(map((posts) => posts.length)));
-    expect(initialLength).toBe(newLength + 1)
-  })
+    let firstPostId = await firstValueFrom(
+      service.allPosts$.pipe(map((posts) => posts[0].id)),
+    );
+    await service.deletePost(firstPostId);
+    let newLength = await firstValueFrom(
+      service.allPosts$.pipe(map((posts) => posts.length)),
+    );
+    expect(initialLength).toBe(newLength + 1);
+  });
 
   it('should edit posts', async () => {
     await getInitialLength();
-    let firstPostId = await firstValueFrom(service.allPosts$.pipe(map((posts) => posts[0].id)))
-    await service.editPost({id: Number(firstPostId), title: "Edited test title", content: "Edited test content"})
-    let editedPost = await firstValueFrom(service.allPosts$.pipe(map((posts) => posts.find((post) => post.id == firstPostId))));
-    expect(editedPost && editedPost.title == "Test title" && editedPost.content == "Test content").toBeTruthy();
-  })
+    let firstPostId = await firstValueFrom(
+      service.allPosts$.pipe(map((posts) => posts[0].id)),
+    );
+    await service.editPost({
+      id: Number(firstPostId),
+      title: 'Edited test title',
+      content: 'Edited test content',
+    });
+    let editedPost = await firstValueFrom(
+      service.allPosts$.pipe(
+        map((posts) => posts.find((post) => post.id == firstPostId)),
+      ),
+    );
+    expect(
+      editedPost &&
+        editedPost.title == 'Test title' &&
+        editedPost.content == 'Test content',
+    ).toBeTruthy();
+  });
 });
