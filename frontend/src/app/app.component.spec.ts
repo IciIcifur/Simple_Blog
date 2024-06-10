305aa555a55a9458e67a63857759d5d1c820f03c
IciIcifur <iciicifur@yandex.ru>
2024-06-10 17:20:05 +0300

    Trying tests action

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {appConfig} from "./app.config";
+import {importProvidersFrom} from "@angular/core";
+import {Apollo} from "apollo-angular";
+import {ApolloProvider} from "@apollo/client";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [appConfig.providers]
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(
+      AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
@@ -25,7 +31,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, frontend',
+      'ОтДуши',
     );
   });
 });
